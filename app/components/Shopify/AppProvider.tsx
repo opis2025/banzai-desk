f04bce86ba617dcce2ff327c5b89fc31141f3182
Jay <opis2025@gmail.com>
2025-05-24 01:29:28 +0900

    Fix JSON import using dynamic import to support esbuild

--- app/components/Shopify/AppProvider.tsx
+++ app/components/Shopify/AppProvider.tsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react";
 import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
-import { ReactNode } from "react";
 import { BrowserRouter } from "react-router-dom";
-import en from "@shopify/polaris/locales/en.json" with { type: "json" };
+import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
 }
 
 export function AppProvider({ children }: Props) {
+  const [i18n, setI18n] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const data = await import("@shopify/polaris/locales/en.json");
+      setI18n(data.default);
+    })();
+  }, []);
+
+  if (!i18n) return null; // 혹은 로딩 스피너 등 표시 가능
+
   return (
-    <PolarisAppProvider i18n={en}>
+    <PolarisAppProvider i18n={i18n}>
       <BrowserRouter>{children}</BrowserRouter>
     </PolarisAppProvider>
   );
